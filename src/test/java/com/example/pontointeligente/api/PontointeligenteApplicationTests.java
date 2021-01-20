f4d7ab5a6a871f0143e509547939429b30c6f3a2
marcelinoNet <marcelinoneto34@gmail.com>
2021-01-20 09:56:11 -0300

    primeiro push

---
+++ src/test/java/com/example/pontointeligente/api/PontointeligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontointeligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
