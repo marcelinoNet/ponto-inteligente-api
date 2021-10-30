d30b9550117fb431d46c54f0bff55112abd24371
marcelinoNet <marcelinoneto34@gmail.com>
2021-10-30 09:05:16 -0300

    commit inicial dos arquivos do projeto

---
+++ src/test/java/com/example/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pontointeligente.api;
+
+import org.junit.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
